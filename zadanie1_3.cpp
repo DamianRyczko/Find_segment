2b4fbc96891d9d55f28d5d4855e97e1d4bb85376
adriangorecki <agorecki200@gmail.com>
2022-02-05 15:01:47 +0100

    refactor and tests

--- zadanie1_3.cpp
+++ zadanie1_3.cpp
@@ -4,54 +4,72 @@
 
 using namespace std;
 
-int segment_value(vector<int> seg){
-    int suma{0};
-    for(int i = 0;i<=seg.size()-1;i++){
-        suma = suma + seg[i];
+int vec_sum(vector<int> seg){
+    int sum{0};
+    for(int i = 0; i < seg.size(); i++){
+        sum = sum + seg[i];
     }
-    return suma;
+    return sum;
 }
-int find_segments(vector<int> tab){
-    int suma;
-    int suma_max = tab[0];
 
-    for(int a=0;a<=tab.size()-1;a++){
+int find_max_sum_segment(vector<int> vec){
+    int max_sum = vec[0];
+
+    for(int a = 0; a<vec.size(); a++){
         vector<int> segment{0};
 
-        for(int j = a;j<=tab.size()-1;j++){
-             segment.push_back(tab[j]);
-            suma = segment_value(segment);
+        for(int j = a; j< vec.size(); j++){
+            segment.push_back(vec[j]);
+            int sum = vec_sum(segment);
 
-                 if(suma>suma_max){
-                    suma_max=suma;
-                    cout<<"now max wartosc: "<<suma_max<<" dla przedialu od "<<a<<" do "<<j+1<<endl;
-                 }
+            if(sum > max_sum){
+                max_sum = sum;
+                // cout << "now max wartosc: " << max_sum << " dla przedialu od "<< a << " do "<< j + 1 << endl;
+            }
         }
-
     }
-    return suma_max;
+    return max_sum;
+}
+
+vector<int> read_file(string path){
+    fstream input_file;
+    input_file.open(path, ios::in);
+    if(!input_file.good()){
+        cout << "File does not exist!";
+    }
+
+    int liczba;
+    vector<int> vec{};
+    while (input_file >> liczba){
+        vec.push_back(liczba);
+    }
+
+    input_file.close();
+
+    return vec;
+}
+
+void test(string path, int expected_result){
+    int max_sum = find_max_sum_segment(read_file(path));
+    if(max_sum == expected_result)
+        cout << "POPRAWNY " << " plik: " << path << " oczekiwano:" << expected_result << " obliczono:" << max_sum << endl;
+    else
+        cout << "BŁĘDNY " << " plik: " << path << " oczekiwano:" << expected_result << " obliczono:" << max_sum << endl;
 }
 
 int main(){
-    fstream input_file;
+    // vector<int> vec = read_file("dane1_3.txt");
+    // int max_sum = find_max_sum_segment(vec);
+    // cout << "Max wartosc sehmentu to: " << max_sum;
 
-    input_file.open("dane1_3.txt", ios::in);
-        if(input_file.good()==false){
-            cout<<"File does not exist!";
-            return 0;
-        }
+    test("test_1.txt", 1);
+    test("test_2.txt", 2);
+    test("test_3.txt", 3);
+    test("test_4.txt", 13);
+    test("test_5.txt", -1);
+    test("test_6.txt", 0);
 
-    int liczba;
-    vector<int> table{};
-
-    while (input_file >> liczba){
-        table.push_back(liczba);
-    }
-    input_file.close();
-
-    int suma_max = find_segments(table);
-
-    cout<<"Max wartosc sehmentu to: "<<suma_max;
+    cin.get();
 
     return 0;
 }
